a41056e800468711381e7dc2cd395e2f1b46a8c5
nikitau-svg <267359443+nikitau-svg@users.noreply.github.com>
2026-08-31 20:16:02 +0400

    release: prepare 0.3.0-beta.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "io.github.nikitau.spruthubhelper"
         minSdk = 30
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.3.0-beta.7"
+        versionCode = 10
+        versionName = "0.3.0-beta.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
