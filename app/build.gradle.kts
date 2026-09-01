c6970a54cbedaaac6f8fa13aabe0d28e81d86734
nikitau-svg <267359443+nikitau-svg@users.noreply.github.com>
2026-09-02 03:40:28 +0400

    chore: prepare 0.7.0-beta.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "io.github.nikitau.spruthubhelper"
         minSdk = 30
         targetSdk = 35
-        versionCode = 32
-        versionName = "0.7.0-beta.6"
+        versionCode = 33
+        versionName = "0.7.0-beta.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
